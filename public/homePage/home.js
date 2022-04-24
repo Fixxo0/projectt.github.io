a0231aa1f1a759eced3b49f75763710dc45fbc46
Raj palmal <rajpalmal40@gmail.com>
2022-04-24 20:47:00 +0530

    first commit

---
+++ public/homePage/home.js
@@ -0,0 +1,7 @@
+var resultt  = document.querySelector('#res'); 
+console.log(resultt);
+function number(value){
+    document.Calculator.result.textContent.value += value;
+}
+
+// console.log(document.Calculator.Result.value);
